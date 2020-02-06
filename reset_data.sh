c06a1e9e1854cc0b698283daf19eefca70554d00
Dave Pederson <dave.pederson@gmail.com>
2020-02-05 19:41:07 -0700

    Ensure kvstore.db dir exists

--- reset_data.sh
+++ reset_data.sh
@@ -4,6 +4,11 @@ TMHOME=./nodes/c2 tendermint unsafe_reset_all
 TMHOME=./nodes/c3 tendermint unsafe_reset_all
 TMHOME=./nodes/c4 tendermint unsafe_reset_all
 
+mkdir -p ./nodes/c1/data/kvstore.db
+mkdir -p ./nodes/c2/data/kvstore.db
+mkdir -p ./nodes/c3/data/kvstore.db
+mkdir -p ./nodes/c4/data/kvstore.db
+
 touch ./nodes/c1/data/.gitkeep
 touch ./nodes/c2/data/.gitkeep
 touch ./nodes/c3/data/.gitkeep
